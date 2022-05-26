db62c992c532899b111338b07d5195ba6e3834f8
shinhosuck <shinhosuck1973@gmail.com>
2022-05-26 09:48:59 -0700

    Delete djcloudmusic directory

--- djcloudmusic/users/admin.py
+++
@@ -1,6 +0,0 @@
-import imp
-from django.contrib import admin
-from users.models import Message
-
-
-admin.site.register(Message)
--- djcloudmusic/users/context_processors.py
+++
@@ -1,6 +0,0 @@
-from users.forms import MessageForm
-
-
-def message_form(request):
-    user_message_form = MessageForm()
-    return {"user_message_form": user_message_form}
