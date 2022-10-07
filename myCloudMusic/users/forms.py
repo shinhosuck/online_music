2f2477e014bda31326fd0d80e3fa58cd0dfef7e9
shinhosuck <shinhosuck1973@gmail.com>
2022-10-06 22:32:37 -0700

    update

--- myCloudMusic/users/forms.py
+++ myCloudMusic/users/forms.py
@@ -11,7 +11,7 @@ class UserRegisterForm(UserCreationForm):
             "username",
             # "first_name",
             # "last_name",
-            # "email",
+            "email",
             "password1",
             "password2"
         ]
